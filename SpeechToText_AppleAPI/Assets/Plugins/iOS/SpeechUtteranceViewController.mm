9d1cf144cb9fe77122ffbceac60c3cbe675289e5
PingAK <khanhuitse05@gmail.com>
2019-11-14 16:21:59 +0700

    -fix bug -update unity 2019.1

--- SpeechToText_AppleAPI/Assets/Plugins/iOS/SpeechUtteranceViewController.mm
+++ SpeechToText_AppleAPI/Assets/Plugins/iOS/SpeechUtteranceViewController.mm
@@ -3,11 +3,11 @@
 
 @interface SpeechUtteranceViewController () <AVSpeechSynthesizerDelegate>
 {
-	AVSpeechSynthesizer *speechSynthesizer;
-	NSString * speakText;
-	NSString * LanguageCode;
-	float pitch;
-	float rate;
+    AVSpeechSynthesizer *speechSynthesizer;
+    NSString * speakText;
+    NSString * LanguageCode;
+    float pitch;
+    float rate;
 }
 @end
 
@@ -15,36 +15,36 @@
 
 - (id)init
 {
-	self = [super init];	
-	speechSynthesizer = [[AVSpeechSynthesizer alloc] init];
+    self = [super init];
+    speechSynthesizer = [[AVSpeechSynthesizer alloc] init];
     speechSynthesizer.delegate = self;
-	return self;
+    return self;
 }
 - (void)SettingSpeak: (const char *) _language pitchSpeak: (float)_pitch rateSpeak:(float)_rate
-{	
-	LanguageCode = [NSString stringWithUTF8String:_language];
-	pitch = _pitch;
+{
+    LanguageCode = [NSString stringWithUTF8String:_language];
+    pitch = _pitch;
     rate = _rate;
     UnitySendMessage("TextToSpeech", "onMessage", "Setting Success");
 }
 - (void)StartSpeak: (const char *) _text
 {
-    speakText = [NSString stringWithUTF8String:_text];
-    AVSpeechUtterance *utterance = [[AVSpeechUtterance alloc] initWithString:speakText];
-    utterance.voice = [AVSpeechSynthesisVoice voiceWithLanguage:LanguageCode];
-    utterance.pitchMultiplier = pitch;
-    utterance.rate = rate;
-    utterance.preUtteranceDelay = 0.2f;
-    utterance.postUtteranceDelay = 0.2f;
+    if([speechSynthesizer isSpeaking] == false) {
+        speakText = [NSString stringWithUTF8String:_text];
+        NSLog(@"%@", speakText);
+        AVSpeechUtterance *utterance = [[AVSpeechUtterance alloc] initWithString:speakText];
+        utterance.voice = [AVSpeechSynthesisVoice voiceWithLanguage:LanguageCode];
+        utterance.pitchMultiplier = pitch;
+        utterance.rate = rate;
+        utterance.preUtteranceDelay = 0.2f;
+        utterance.postUtteranceDelay = 0.2f;
 
-    [speechSynthesizer speakUtterance:utterance];
+        [speechSynthesizer speakUtterance:utterance];
+    }
 }
 - (void)StopSpeak
 {
     if([speechSynthesizer isSpeaking]) {
-        [speechSynthesizer stopSpeakingAtBoundary:AVSpeechBoundaryImmediate];
-        AVSpeechUtterance *utterance = [AVSpeechUtterance speechUtteranceWithString:@""];
-        [speechSynthesizer speakUtterance:utterance];
         [speechSynthesizer stopSpeakingAtBoundary:AVSpeechBoundaryImmediate];
     }
 }
@@ -57,7 +57,7 @@ willSpeakRangeOfSpeechString:(NSRange)characterRange
     UnitySendMessage("TextToSpeech", "onSpeechRange", [subString UTF8String]);
 }
 
-- (void)speechSynthesizer:(AVSpeechSynthesizer *)synthesizer 
+- (void)speechSynthesizer:(AVSpeechSynthesizer *)synthesizer
 didStartSpeechUtterance:(AVSpeechUtterance *)utterance
 {
     UnitySendMessage("TextToSpeech", "onStart", "onStart");
@@ -72,14 +72,14 @@ didStartSpeechUtterance:(AVSpeechUtterance *)utterance
 @end
 
 extern "C"{
-    SpeechUtteranceViewController *su = [[SpeechUtteranceViewController alloc] init];  
+    SpeechUtteranceViewController *su = [[SpeechUtteranceViewController alloc] init];
     void _TAG_StartSpeak(const char * _text){
         [su StartSpeak:_text];
     }
-	void _TAG_StopSpeak(){
+    void _TAG_StopSpeak(){
         [su StopSpeak];
-    } 
-	void _TAG_SettingSpeak(const char * _language, float _pitch, float _rate){
+    }
+    void _TAG_SettingSpeak(const char * _language, float _pitch, float _rate){
         [su SettingSpeak:_language pitchSpeak:_pitch rateSpeak:_rate];
-    }    
+    }
 }
