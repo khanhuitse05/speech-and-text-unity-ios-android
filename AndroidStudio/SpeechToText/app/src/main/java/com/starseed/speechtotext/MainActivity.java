f354d6a57443905c49cdf29cd259008186da720b
Udayshankar Ravikumar <uralstech@gmail.com>
2023-09-02 02:15:06 +0530

    Updated to latest Android API level.

--- AndroidStudio/SpeechToText/app/src/main/java/com/starseed/speechtotext/MainActivity.java
+++ AndroidStudio/SpeechToText/app/src/main/java/com/starseed/speechtotext/MainActivity.java
@@ -1,17 +1,17 @@
 package com.starseed.speechtotext;
 
-import android.content.Intent;
+import android.util.Log;
 import android.os.Bundle;
-import android.speech.RecognitionListener;
+import android.content.Intent;
 import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
 import android.speech.tts.TextToSpeech;
+import android.speech.RecognitionListener;
 import android.speech.tts.UtteranceProgressListener;
 import com.unity3d.player.UnityPlayer;
 import com.unity3d.player.UnityPlayerActivity;
-import java.util.ArrayList;
 import java.util.Locale;
-
+import java.util.ArrayList;
 
 public class MainActivity extends UnityPlayerActivity
 {
@@ -40,10 +40,17 @@ public class MainActivity extends UnityPlayerActivity
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+        //super.onActivityResult(requestCode, resultCode, data);
         if (resultCode == RESULT_OK && null != data) {
-            ArrayList<String> text = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-            UnityPlayer.UnitySendMessage("SpeechToText", "onResults", text.get(0));
+            try
+            {
+                ArrayList<String> text = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                UnityPlayer.UnitySendMessage("SpeechToText", "onResults", text.get(0));
+            }
+            catch(NullPointerException e)
+            {
+                Log.i("SpeechToText", "NullPointerException from onActivityResult: " + e.getMessage());
+            }
         }
     }
 
@@ -53,11 +60,24 @@ public class MainActivity extends UnityPlayerActivity
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_PREFERENCE, Bridge.languageSpeech);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, Bridge.languageSpeech);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Bridge.languageSpeech);
+        intent.putExtra(RecognizerIntent.EXTRA_PARTIAL_RESULTS, true);
         //intent.putExtra(RecognizerIntent.EXTRA_SPEECH_INPUT_MINIMUM_LENGTH_MILLIS, 2000);
         intent.putExtra(RecognizerIntent.EXTRA_SPEECH_INPUT_COMPLETE_SILENCE_LENGTH_MILLIS, 2000);
         //intent.putExtra(RecognizerIntent.EXTRA_SPEECH_INPUT_POSSIBLY_COMPLETE_SILENCE_LENGTH_MILLIS, 2000);
         intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, this.getPackageName());
         intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 3);
+        //onLog("test");
+
+        /*Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, languageSpeech);
+        intent.putExtra(RecognizerIntent.EXTRA_PARTIAL_RESULTS, true);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_PREFERENCE, languageSpeech);
+        intent.putExtra(RecognizerIntent.EXTRA_ONLY_RETURN_LANGUAGE_PREFERENCE, languageSpeech);
+        intent.putExtra(RecognizerIntent.EXTRA_SPEECH_INPUT_MINIMUM_LENGTH_MILLIS, Long.valueOf(5000));
+        intent.putExtra(RecognizerIntent.EXTRA_SPEECH_INPUT_COMPLETE_SILENCE_LENGTH_MILLIS, Long.valueOf(3000));
+        intent.putExtra(RecognizerIntent.EXTRA_SPEECH_INPUT_POSSIBLY_COMPLETE_SILENCE_LENGTH_MILLIS, Long.valueOf(3000));
+        if (!prompt.equals("")) intent.putExtra(RecognizerIntent.EXTRA_PROMPT, prompt);
+        UnityPlayer.currentActivity.startActivityForResult(intent, RESULT_SPEECH);*/
 
         this.runOnUiThread(new Runnable() {
 
@@ -80,6 +100,7 @@ public class MainActivity extends UnityPlayerActivity
     }
 
     RecognitionListener recognitionListener = new RecognitionListener() {
+
         @Override
         public void onReadyForSpeech(Bundle params) {
             UnityPlayer.UnitySendMessage("SpeechToText", "onReadyForSpeech", params.toString());
@@ -90,7 +111,7 @@ public class MainActivity extends UnityPlayerActivity
         }
         @Override
         public void onRmsChanged(float rmsdB) {
-            UnityPlayer.UnitySendMessage("SpeechToText", "onRmsChanged", "" + rmsdB);
+            //UnityPlayer.UnitySendMessage("SpeechToText", "onRmsChanged", "" + rmsdB);
         }
         @Override
         public void onBufferReceived(byte[] buffer) {
@@ -106,13 +127,33 @@ public class MainActivity extends UnityPlayerActivity
         }
         @Override
         public void onResults(Bundle results) {
-            ArrayList<String> text = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
-            UnityPlayer.UnitySendMessage("SpeechToText", "onResults", text.get(0));
+            try
+            {
+                ArrayList<String> text = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+                UnityPlayer.UnitySendMessage("SpeechToText", "onResults", text.get(0));
+            }
+            catch (NullPointerException e)
+            {
+                Log.i("SpeechToText", "NullPointerException from onResults: " + e.getMessage());
+            }
+
+            UnityPlayer.UnitySendMessage("SpeechToText", "onResults", "");
         }
         @Override
         public void onPartialResults(Bundle partialResults) {
-            ArrayList<String> text = partialResults.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
-            UnityPlayer.UnitySendMessage("SpeechToText", "onPartialResults", text.get(0));
+            try
+            {
+                ArrayList<String> text = partialResults.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+                UnityPlayer.UnitySendMessage("SpeechToText", "onPartialResults", text.get(0));
+            }
+            catch(NullPointerException e)
+            {
+                Log.i("SpeechToText", "NullPointerException from onPartialResults: " + e.getMessage());
+            }
+            catch(IndexOutOfBoundsException i)
+            {
+                Log.i("SpeechToText", "IndexOutOfBoundsException from onPartialResults: " + i.getMessage());
+            }
         }
         @Override
         public void onEvent(int eventType, Bundle params) {
